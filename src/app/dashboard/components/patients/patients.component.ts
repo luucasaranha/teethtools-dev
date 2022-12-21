ae7af5d821dd86afc75aec7ffa1decad3a3c040d
João Friedrich <joaopedrohf@hotmail.com>
2022-12-20 21:42:37 -0300

    Refactor patients

--- src/app/dashboard/components/patients/patients.component.ts
+++ src/app/dashboard/components/patients/patients.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
-import {Observable, of} from 'rxjs';
 import {PatientsService} from '../../services/list-patient/patients.service';
 import {DeletePatientService} from "../../services/delete-patient/delete-patient.service";
-import {UpdatePatientService} from '../../services/update-patient/update-patient.service';
-import {FirebaseApp} from "@angular/fire/app";
 import {Patient} from "../../model/Patient";
 import {User} from "../../model/user";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-patients',
@@ -14,12 +14,6 @@ import {User} from "../../model/user";
   styleUrls: ['./patients.component.scss'],
 })
 export class PatientsComponent implements OnInit {
-
-  public patientsList: Observable<Patient[]> = new Observable<Patient[]>();
-  private rawPatientsList: Patient[] = []
-
-  public searchText: string = ''
-
   public displayedColumns = [
     'name',
     'gender',
@@ -30,35 +24,31 @@ export class PatientsComponent implements OnInit {
     'actions',
   ];
 
+  dataSource!: MatTableDataSource<Patient>;
+
+  @ViewChild('paginator') paginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+
   constructor(
     private patientService: PatientsService,
     private deletePatientService: DeletePatientService,
-    private updatePatientService: UpdatePatientService,
-    private firebase: FirebaseApp,
     private router: Router,
   ) {
-    this.getPatients()
   }
 
-  private getPatients() {
-    this.patientsList = this.patientService
-      .getPatientsAuthenticateMode(
-        new User('dennis', 'instdenis8569')
-      );
-
-    this.patientsList.subscribe(patient => {
-      this.rawPatientsList = patient
-    })
+  ngOnInit() {
+    this.loadList()
   }
 
-  ngOnInit(): void {}
+  filterData($event: any) {
+    this.dataSource.filter = $event.target.value;
+  }
 
   public onAdd() {
     this.navigateToCadastroPage()
   }
 
   public onUpdate(element: any) {
-    console.log(JSON.stringify(element))
     this.navigateToUpdatePage(element)
   }
 
@@ -67,7 +57,7 @@ export class PatientsComponent implements OnInit {
       .subscribe({
         complete: () => {
           alert("Paciente " + element['name'] + " deletado com sucesso")
-          this.reloadList()
+          this.loadList()
         },
         error: err => {
           alert("Ocorreu um erro ao deletar o paciente")
@@ -76,30 +66,18 @@ export class PatientsComponent implements OnInit {
       })
   }
 
-  public onFilterByName(element: any) {
-    let filteredList: Patient[] = []
-    let name = element.target.value.toLowerCase()
-
-    this.rawPatientsList.filter(patient => {
-      if (patient.name.toLowerCase().includes(name)) {
-        filteredList.push(patient)
-      }
-    })
-
-    this.patientsList = of(filteredList)
-  }
-
-  private reloadList() {
-    this.patientService.getPatients().subscribe({
+  private loadList() {
+    (this.patientService.getPatientsAuthenticateMode(new User('dennis', 'instdenis8569')).subscribe({
       next: (response) => {
-        this.rawPatientsList = response
-        this.patientsList = of(response)
+        this.dataSource = new MatTableDataSource(response);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.matSort;
       },
       error: (error) => {
         alert("Ocorreu um erro, tente novamente mais tarde")
         console.log(error)
       }
-    })
+    }))
   }
 
   private navigateToCadastroPage() {
