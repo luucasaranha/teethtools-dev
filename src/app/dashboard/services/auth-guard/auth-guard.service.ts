7865f4aebd1621aede5dd8dc91117157aef9654f
Lucas Aranha <lucinhasaranha@gmail.com>
2023-02-14 18:47:30 -0300

    fix(auth-guard): removing unused parameter from constructor

--- src/app/dashboard/services/auth-guard/auth-guard.service.ts
+++ src/app/dashboard/services/auth-guard/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRoute, CanActivate, Router} from "@angular/router";
+import {CanActivate, Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,6 @@ export class AuthGuardService implements CanActivate{
   private readonly LOGGED_IN_KEY: string ="loggedIn";
 
   constructor(
-    private route:ActivatedRoute,
     private router: Router
   ) { }
 
@@ -18,7 +17,6 @@ export class AuthGuardService implements CanActivate{
         sessionStorage.getItem(this.LOGGED_IN_KEY) === null ||
         sessionStorage.getItem(this.LOGGED_IN_KEY) === "false"
       ) {
-        console.log("nao esta logado")
         this.router.navigate(['login'])
         return false
       }
