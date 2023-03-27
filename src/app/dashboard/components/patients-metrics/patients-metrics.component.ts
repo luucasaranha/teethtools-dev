5f8e3c107c4612a017f2725f38f9b2cf8ba654b1
Lucas Aranha <lucinhasaranha@gmail.com>
2023-03-27 19:11:26 -0300

    feat (patient-metrics): adding some content in html

--- src/app/dashboard/components/patients-metrics/patients-metrics.component.ts
+++ src/app/dashboard/components/patients-metrics/patients-metrics.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PatientMetricsService} from "../../services/patient-metrics/patient-metrics.service";
 
 @Component({
   selector: 'app-patients-metrics',
   templateUrl: './patients-metrics.component.html',
   styleUrls: ['./patients-metrics.component.scss']
 })
-export class PatientsMetricsComponent {
+export class PatientsMetricsComponent implements OnInit{
+
+
+  constructor(private metricsService: PatientMetricsService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+
 
 }
